20c88b80b248a22da60b53a28a16acefb71ac199
Leolieu02 <s364570@oslomet.no>
2023-02-10 13:54:51 +0100

    Initial commit, med ferdig med å enhetsteste kundecontroller

---
+++ src/test/java/oslomet/testing/EnhetstestBankController.java
@@ -0,0 +1,126 @@
+package oslomet.testing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import oslomet.testing.API.BankController;
+import oslomet.testing.DAL.BankRepository;
+import oslomet.testing.Models.Konto;
+import oslomet.testing.Models.Kunde;
+import oslomet.testing.Models.Transaksjon;
+import oslomet.testing.Sikkerhet.Sikkerhet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class EnhetstestBankController {
+
+    @InjectMocks
+    // denne skal testes
+    private BankController bankController;
+
+    @Mock
+    // denne skal Mock'es
+    private BankRepository repository;
+
+    @Mock
+    // denne skal Mock'es
+    private Sikkerhet sjekk;
+
+    @Test
+    public void hentTransaksjonerOK(){
+        List <Transaksjon> transaksjoner = new ArrayList<>();
+        Konto enKonto = new Konto("12345678910", "12345", 14.99, "Brukskonto", "NOK", transaksjoner);
+
+        when(sjekk.loggetInn()).thenReturn("12345678910");
+
+        when(repository.hentTransaksjoner("12345", "12-11-2022", "15-11-2022")).thenReturn(enKonto);
+
+        // Act
+        Konto resultat = bankController.hentTransaksjoner("12345", "12-11-2022", "15-11-2022");
+
+        assertEquals(enKonto, resultat);
+    }
+
+    @Test
+    public void hentTransaksjonerFeil(){
+
+    }
+
+    @Test
+    public void hentKundeInfo_loggetInn() {
+
+        // arrange
+        Kunde enKunde = new Kunde("01010110523",
+                "Lene", "Jensen", "Askerveien 22", "3270",
+                "Asker", "22224444", "HeiHei");
+
+        when(sjekk.loggetInn()).thenReturn("01010110523");
+
+        when(repository.hentKundeInfo(anyString())).thenReturn(enKunde);
+
+        // act
+        Kunde resultat = bankController.hentKundeInfo();
+
+        // assert
+        assertEquals(enKunde, resultat);
+    }
+
+    @Test
+    public void hentKundeInfo_IkkeloggetInn() {
+
+        // arrange
+        when(sjekk.loggetInn()).thenReturn(null);
+
+        //act
+        Kunde resultat = bankController.hentKundeInfo();
+
+        // assert
+        assertNull(resultat);
+    }
+
+    @Test
+    public void hentKonti_LoggetInn()  {
+        // arrange
+        List<Konto> konti = new ArrayList<>();
+        Konto konto1 = new Konto("105010123456", "01010110523",
+                720, "Lønnskonto", "NOK", null);
+        Konto konto2 = new Konto("105010123456", "12345678901",
+                1000, "Lønnskonto", "NOK", null);
+        konti.add(konto1);
+        konti.add(konto2);
+
+        when(sjekk.loggetInn()).thenReturn("01010110523");
+
+        when(repository.hentKonti(anyString())).thenReturn(konti);
+
+        // act
+        List<Konto> resultat = bankController.hentKonti();
+
+        // assert
+        assertEquals(konti, resultat);
+    }
+
+    @Test
+    public void hentKonti_IkkeLoggetInn()  {
+        // arrange
+        when(sjekk.loggetInn()).thenReturn(null);
+
+        // act
+        List<Konto> resultat = bankController.hentKonti();
+
+        // assert
+        assertNull(resultat);
+    }
+}
+
---
+++ src/test/java/oslomet/testing/EnhetstestKundeController.java
@@ -0,0 +1,171 @@
+package oslomet.testing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import oslomet.testing.API.AdminKundeController;
+import oslomet.testing.DAL.AdminRepository;
+import oslomet.testing.Models.Kunde;
+import oslomet.testing.Sikkerhet.Sikkerhet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class EnhetstestKundeController {
+    @InjectMocks
+    // tester kundecontrolleren
+    private AdminKundeController kundeController;
+
+    @Mock
+    // mocker Adminrepository
+    private AdminRepository repository;
+
+    @Mock
+    // mocker sikkerhet, for å sjekke logg inn status
+    private Sikkerhet sjekk;
+
+    @Test
+    public void hentAlle_OK(){
+        // arrange
+        List <Kunde> kunder = new ArrayList<>(); // lager listen som skal returneres
+        Kunde kunde1 = new Kunde("12345678910", "Per", "Hansen", "Osloveien 2",
+                "0123", "Oslo", "10203040", "Hei123"); // lager en kunde som skal være "logget inn"
+        Kunde kunde2 = new Kunde("10987654321", "Olav", "Hansen", "Osloveien 10", "0123"
+                , "Oslo", "40302010", "Hei123"); // lager en kunde som skal være "logget inn"
+        kunder.add(kunde1);
+        kunder.add(kunde2);
+
+        when(sjekk.loggetInn()).thenReturn("12345678910");
+
+        when(repository.hentAlleKunder()).thenReturn(kunder);
+
+        // act
+        List <Kunde> kundeResultat = kundeController.hentAlle();
+
+        //assert
+        assertEquals(kunder, kundeResultat);
+    }
+
+    @Test
+    public void hentAlle_ikkeLoggetInn(){
+        // arrange
+        when(sjekk.loggetInn()).thenReturn(null);
+
+        // act
+        List <Kunde> kundeResultat = kundeController.hentAlle();
+
+        // assert
+        assertNull(kundeResultat);
+    }
+
+    @Test
+    public void lagreKunde_loggetInn(){
+        // arrange
+        Kunde kunde1 = new Kunde("12345678910", "Per", "Hansen", "Osloveien 2",
+                "0123", "Oslo", "10203040", "Hei123"); // lager en kunde som skal lagres
+        String riktig = "OK"; // strengen som returneres om det er riktig
+
+        when(sjekk.loggetInn()).thenReturn("12345678900");
+
+        when(repository.registrerKunde(any())).thenReturn(riktig);
+
+        // act
+        String resultat = kundeController.lagreKunde(kunde1);
+
+        // assert
+        assertEquals(riktig, resultat);
+    }
+
+    @Test
+    public void lagreKunde_ikkeLoggetInn(){
+        // arrange
+        Kunde kunde1 = new Kunde("12345678910", "Per", "Hansen", "Osloveien 2",
+                "0123", "Oslo", "10203040", "Hei123"); // lager en kunde som skal lagres
+        when(sjekk.loggetInn()).thenReturn(null);
+
+        // act
+        String resultat = kundeController.lagreKunde(kunde1);
+
+        // assert
+        assertEquals("Ikke logget inn", resultat);
+    }
+
+    @Test
+    public void endreKunde_loggetInn(){
+        // arrange
+        String riktig = "OK";
+        Kunde kunde1 = new Kunde("12345678910", "Per", "Hansen", "Osloveien 2",
+                "0123", "Oslo", "10203040", "Hei123"); // lager en kunde som skal lagres
+
+        when(sjekk.loggetInn()).thenReturn("12345678900");
+
+        when(repository.endreKundeInfo(any())).thenReturn(riktig);
+
+        // act
+        String resultat = kundeController.endre(kunde1);
+
+        // assert
+        assertEquals(riktig, resultat);
+    }
+
+    @Test
+    public void endreKunde_ikkeLoggetInn(){
+        // arrange
+        Kunde kunde1 = new Kunde("12345678910", "Per", "Hansen", "Osloveien 2",
+                "0123", "Oslo", "10203040", "Hei123"); // lager en kunde som skal lagres
+        when(sjekk.loggetInn()).thenReturn(null);
+
+        // act
+        String resultat = kundeController.endre(kunde1);
+
+        // assert
+        assertEquals("Ikke logget inn", resultat);
+    }
+
+
+    @Test
+    public void slett_loggetInn(){
+        // arrange
+        String riktig = "OK";
+        String kontonummer = "1020304050"; // kontonummer som skal slettes
+
+        when(sjekk.loggetInn()).thenReturn("12345678910"); // sånn at den er logger inn
+
+        when(repository.slettKunde(anyString())).thenReturn(riktig);
+
+        // act
+        String resultat = kundeController.slett(kontonummer);
+
+        // assert
+        assertEquals(riktig, resultat);
+    }
+
+
+    @Test
+    public void slett_ikkeLoggetInn(){
+        // arrange
+        String kontonummer = "10203040";
+        when(sjekk.loggetInn()).thenReturn(null);
+
+        // act
+        String resultat = kundeController.slett(kontonummer);
+
+        // assert
+        assertEquals("Ikke logget inn", resultat);
+    }
+
+
+
+
+
+
+}
